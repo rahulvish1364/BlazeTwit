49810350cc91538c3a11f380410fbfb46bd41d89
RAHUL VISHWAKARMA <33103549+rahulvish1364@users.noreply.github.com>
2020-05-09 19:49:45 +0530

    Addedd code for fetching info
    Opning the browser window and get api keys

--- command/configure.js
+++ command/configure.js
@@ -1,6 +1,10 @@
 const inquirer = require('inquirer')
 const CredentialManager = require('../lib/credential-manager');
 const util  = require('../lib/util');
+const Twitter = require('../lib/twitter');
+const opn = require('opn');
+const querystring = require('querystring');
+const { notEmpty } = require('../lib/util');
 const configure = {
      async consumer(name){
         let creds = new CredentialManager(name)
@@ -19,7 +23,39 @@ const configure = {
             }
         ])
       await creds.storeKeyAndSecret('apiKey', answer.key, answer.secret)  
-     }
+     },
+     async account(name){
+        let creds = new CredentialManager(name)
+        var [apiKey, apiSecret] = await creds.getKeyAndSecret('apiKey')
+        let twitter = new Twitter(apiKey, apiSecret)
+        let response = querystring.parse(await twitter.post('oauth/request_token'))
+        twitter.setToken(response['oauth_token'], response['oauth_token_secret'])
+        await inquirer.prompt({
+            type: 'input',
+            message: 'Press enter to open the twitter in you default browser to authorize access',
+            name: 'continue'
+        })
+        opn(`${twitter.baseUrl}oauth/authorize?oauth_token=${response['oauth_token']}`)
+        let answer = await inquirer.prompt({
+            type: 'input',
+            message: 'Enter the Pin provided',
+            name: 'pin',
+            validate: util.notEmpty
+        })
+        let tokenRespone = querystring.parse(
+            await twitter.post('oauth/access_token', `oauth_verifier=${answer.pin}`)
+        )
+        twitter.setToken(tokenRespone['oauth_token'], tokenRespone['oauth_token_secret'])
+        let verifyResponse = await twitter.get('1.1/account/verify_credentials.json')
+        await creds.storeKeyAndSecret(
+            'accountToken',
+            tokenRespone['oauth_token'],
+            tokenRespone['oauth_token_secret']
+        )
+        console.log(`Account "${verifyResponse['screen_name']}" added successfully`);
+        
+    }
+    
 }
 
 module.exports = configure
