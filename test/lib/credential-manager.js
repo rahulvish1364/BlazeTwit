d72153354c5884eb1a4b5b86a2ef7791d073de8b
RAHUL VISHWAKARMA <33103549+rahulvish1364@users.noreply.github.com>
2020-05-24 01:40:21 +0530

    Added functanilty to credntials from ENV variables
    Added functionality take the credentials from environtmental variable and also priorties it if saved as evironment variable.

--- test/lib/credential-manager.js
+++ test/lib/credential-manager.js
@@ -11,7 +11,19 @@ describe('a credential manager', () => {
     before(()=>{
         creds = new CredentialManager('blaze-test')
     })
-
+    it('should return the credentials saved in the environment', async () => {
+        process.env['BLAZE-TEST_COMSUER_KEY'] = 'one'
+        process.env['BLAZE-TEST_COMSUER_SECRET'] = 'two'
+        let [key, secret] = await creds.getKeyAndSecret('consumer')
+        expect(key).to.equal('one');
+        expect(secret).to.equal('two');
+    });
+    it('should prefer crednetials saved in the environment', async () => {
+        await creds.storeKeyAndSecret('consumer', 'foo', 'bar')
+        let [key, secret] = await creds.getKeyAndSecret('consumer')
+        expect(key).to.equal('one')
+        expect(secret).to.equal('two');
+    });
     it('should return credential when they are found', async () => {
         await creds.storeKeyAndSecret('apiKey','foo', 'bar')
         let [key, secret] = await creds.getKeyAndSecret('apiKey')
